e8a3387c665d8d922aeb29e4ac4ca50715492fd8
lukkascost <lucas.costa@lit.ifce.edu.br>
2025-02-05 14:41:42 -0300

    first commit

---
+++ src/main/java/br/com/goods/Goods/Investimentos/GoodsInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.goods.Goods.Investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goods/Goods/Investimentos/GoodsInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goods.Goods.Investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
