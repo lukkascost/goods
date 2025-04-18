0cd268dcdd1f10755aa765bd25da68b18d40c7d5
lukkascost <lucas.costa@lit.ifce.edu.br>
2025-04-17 22:50:40 -0300

    ✨ feat(database): Adiciona coluna de ID e restrição única à tabela `lancamento_etf_inter`.

--- src/main/resources/db/migration/dados/V1_0_4__create_table_acoes_inter.sql
+++ src/main/resources/db/migration/dados/V1_0_4__create_table_acoes_inter.sql
@@ -10,7 +10,8 @@ create table lancamento_etf_inter (
     origem character varying(10) not null,
     preco_medio_antes_operacao numeric(20,8) not null
 );
-
+ALTER TABLE lancamento_etf_inter ADD COLUMN id SERIAL;
+ALTER TABLE lancamento_etf_inter ADD CONSTRAINT lancamento_etf_inter_id_unique UNIQUE (id, time);
 CREATE INDEX lancamento_etf_inter_usuario_idx ON lancamento_etf_inter ( id_usuario, time DESC);
 
 SELECT create_hypertable('lancamento_etf_inter', by_range('time', INTERVAL '30 days'));
